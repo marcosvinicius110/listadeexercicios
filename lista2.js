5f50266fe4afa89a334b76ffaff970b2856e64c3
Vinizix11 <marcos.monteiro.moreira@escola.pr.gov.br>
2026-02-27 13:11:47 +0000

    lista exercicio

---
+++ lista2.js
@@ -0,0 +1,29 @@
+//Criar um programa que mostre se um número é positivo ou negativo 
+
+let num1 = 99
+if(num1>=0){
+    console.log (`o numero ${num1} e positivo`)
+} else {
+    console.log (`o numero ${num1} e negativo`)
+}
+
+//Criar um progama que mostre se um ano é bissexto.
+let ano = 2020;
+if (ano % 4 === 0) {
+    console.log ('é bissexto.')
+} else {
+    console.log ('não é bissexto.');
+}
+
+//Criar um programa que mostre uma senha e a mensagem acesso permitido
+
+let senha = "toscão67sixseven"
+if(senha=== "toscão67sixseven"){
+    console.log ("acesso permitido")
+}else {
+    console.log ("acesso negado")
+}
+
+//Criar um programa que mostre um desconto de 5% para compras maiores que R$100
+//Criar um programa que classifique uma pessoa como Criança, adolescente ou adulto de acordo com a idade
+//Criar um programa que mostre qual tipo de triângulo de acordo 
